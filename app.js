59a8f3be8cef05b0680294087c38be10661dd170
IAN MIGLIN <48313356@ort>
2025-03-31 14:50:21 -0300

    Avances js

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ let listaTareas = document.getElementById("listaTareas");
 let tareas = document.getElementById("Tareas");
 let boton = document.getElementById("Boton");
 let botonEliminar = document.getElementById("BotonEliminar")
-
 boton.addEventListener("click", EnviarTarea);  
-botonEliminar.addEventListener("click", EliminarTarea);  
-
+botonBorrar.addEventListener("click", EliminarTarea);  
 
 function EnviarTarea(event) {
     event.preventDefault();  
@@ -14,15 +12,32 @@ function EnviarTarea(event) {
     {  
         AbleButton()
         let li = document.createElement("li");
-        li.textContent = tareas.value;  
+        let botonBorrar = document.createElement("button");
+        const checkbox = document.createElement("input")
+        checkbox.setAttribute("type", "checkbox")
+        botonBorrar.innerHTML = "Borrar"
+        li.textContent = tareas.value;
+        li.appendChild(checkbox);
+        li.appendChild(botonBorrar);  
         listaTareas.appendChild(li);
         tareas.value = "";  
+
+        botonBorrar.addEventListener('click', () => {
+            listaTareas.removeChild(li)
+        })
+
+        botonEliminar.addEventListener('click', () => {
+            const checkBox = li.querySelector(`input[type="checkbox"]`)
+            if(checkBox && checkBox.checked){
+                listaTareas.removeChild(li)
+            }
+        })
+    }
+    else{
+    
+        DisableButton();
     }
     
-}
-function EliminarTarea ()
-{
-    listaTareas.remove()
 }
 
 function DisableButton() {
