362b3ddacbb913556adfefb8ae49760838841a21
IAN MIGLIN <48313356@ort>
2025-03-31 15:49:53 -0300

    Avances storage

--- app.js
+++ app.js
@@ -1,49 +1,52 @@
 let listaTareas = document.getElementById("listaTareas");
 let tareas = document.getElementById("Tareas");
 let boton = document.getElementById("Boton");
-let botonEliminar = document.getElementById("BotonEliminar")
-boton.addEventListener("click", EnviarTarea);  
-botonBorrar.addEventListener("click", EliminarTarea);  
+let botonEliminar = document.getElementById("BotonEliminar");
+boton.addEventListener("click", () => EnviarTarea());
+botonBorrar.addEventListener("click", EliminarTarea);
 
-function EnviarTarea(event) {
-    event.preventDefault();  
+function EnviarTarea(id = new Date().toLocaleString(), value = tareas.value) {
+  if (value.trim() !== "") {
+    AbleButton();
+    new Date();
+    let li = document.createElement("li");
+    let botonBorrar = document.createElement("button");
+    const checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    botonBorrar.innerHTML = "Borrar";
+    li.textContent = value;
+    localStorage.setItem(id, value);
+    li.appendChild(checkbox);
+    li.appendChild(botonBorrar);
+    listaTareas.appendChild(li);
+    tareas.value = "";
 
-    if (tareas.value.trim() !== "") 
-    {  
-        AbleButton()
-        let li = document.createElement("li");
-        let botonBorrar = document.createElement("button");
-        const checkbox = document.createElement("input")
-        checkbox.setAttribute("type", "checkbox")
-        botonBorrar.innerHTML = "Borrar"
-        li.textContent = tareas.value;
-        li.appendChild(checkbox);
-        li.appendChild(botonBorrar);  
-        listaTareas.appendChild(li);
-        tareas.value = "";  
+    botonBorrar.addEventListener("click", () => {
+      listaTareas.removeChild(li);
+    });
 
-        botonBorrar.addEventListener('click', () => {
-            listaTareas.removeChild(li)
-        })
-
-        botonEliminar.addEventListener('click', () => {
-            const checkBox = li.querySelector(`input[type="checkbox"]`)
-            if(checkBox && checkBox.checked){
-                listaTareas.removeChild(li)
-            }
-        })
-    }
-    else{
-    
-        DisableButton();
-    }
-    
+    botonEliminar.addEventListener("click", () => {
+      const checkBox = li.querySelector(`input[type="checkbox"]`);
+      if (checkBox.checked) {
+        listaTareas.removeChild(li);
+      }
+    });
+  } else {
+    DisableButton();
+  }
 }
 
 function DisableButton() {
-    boton.disabled = true;
+  boton.disabled = true;
 }
 
 function AbleButton() {
-    boton.disabled = false;
+  boton.disabled = false;
+}
+
+// Traer los valores del localstorage
+for (let i = 0; i < localStorage.length; i++) {
+  const key = localStorage.key(i);
+  const value = localStorage.getItem(key);
+  EnviarTarea(key, value);
 }
