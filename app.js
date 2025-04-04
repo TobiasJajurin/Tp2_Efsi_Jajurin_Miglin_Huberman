eeb9461a34bb9de074e01218ac04ba5628fc028a
IAN MIGLIN <48313356@ort>
2025-04-04 09:05:37 -0300

    Avances local storage

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let tareas = document.getElementById("Tareas");
 let boton = document.getElementById("Boton");
 let botonEliminar = document.getElementById("BotonEliminar");
 boton.addEventListener("click", () => EnviarTarea());
+let chequeado = false;
 
 function EnviarTarea(id = new Date().toLocaleString(), value = tareas.value) {
   if (value.trim() !== "") {
@@ -11,9 +12,21 @@ function EnviarTarea(id = new Date().toLocaleString(), value = tareas.value) {
     let botonBorrar = document.createElement("button");
     const checkbox = document.createElement("input");
     checkbox.setAttribute("type", "checkbox");
+    checkbox.checked = (localStorage?.getItem(id)?.split(";")[1] ?? "false") !== "false";
     botonBorrar.innerHTML = "Borrar";
-    li.textContent = value;
-    localStorage.setItem(id, value);
+
+    checkbox.addEventListener("change", () => {
+      let valor = localStorage.getItem(id).split(";");
+      valor[1] = checkbox.checked;
+      console.log(checkbox.checked, id);
+      localStorage.setItem(id, valor.join(";"));
+    });
+
+    li.textContent = value.split(";")[0];
+    localStorage.setItem(
+      id,
+      value + (value.split(";").length != 2 ? ";false" : "")
+    );
     li.appendChild(checkbox);
     li.appendChild(botonBorrar);
     listaTareas.appendChild(li);
@@ -21,19 +34,16 @@ function EnviarTarea(id = new Date().toLocaleString(), value = tareas.value) {
 
     botonBorrar.addEventListener("click", () => {
       listaTareas.removeChild(li);
-      localStorage.removeItem(id)
+      localStorage.removeItem(id, value);
     });
 
     botonEliminar.addEventListener("click", () => {
       const checkBox = li.querySelector(`input[type="checkbox"]`);
       if (checkBox.checked) {
         listaTareas.removeChild(li);
+        localStorage.removeItem(id, value);
       }
     });
-    
-      
-
-
   } else {
     DisableButton();
   }
@@ -46,7 +56,6 @@ function DisableButton() {
 function AbleButton() {
   boton.disabled = false;
 }
-
 
 for (let i = 0; i < localStorage.length; i++) {
   const key = localStorage.key(i);
